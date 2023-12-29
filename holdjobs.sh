7d6c1d0cf1549e9d000458e0cfa4d82c8e67ee7d
Suzanna Jean Parkinson <suzannastep@gmail.com>
2023-12-29 09:09:24 -0600

    running for more epochs with label noise

--- holdjobs.sh
+++ holdjobs.sh
@@ -2,5 +2,5 @@
 
 # for i in $(seq 106428 106509); do scontrol hold $i; done
 # for i in $(seq 106428 106509); do scontrol release $i; done
-# for i in $(seq 107219 107224); do scancel $i; done
-squeue -u $USER | grep 197 | awk '{print $1}' | xargs -n 1 scancel
+# for i in $(seq 108445 108508); do scancel $i; done
+# squeue -u $USER | grep 197 | awk '{print $1}' | xargs -n 1 scancel
